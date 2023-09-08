4c7eacd7290a680ff406b9d996ee9f2bfe13fcab
merupulu2 <142010681+merupulu2@users.noreply.github.com>
2023-09-08 22:39:24 +0530

    Update dashboard.js
    implemented not go back after logout

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -198,7 +198,7 @@ dashRouter.get('/admin/allAdmins', isAuth, async (req, res) => {
   dashRouter.get("/logout",(req, res) => {
     req.session.destroy((err) => {
       if (err) throw err;
-      res.redirect("/");
+      res.header('Cache-Control', 'no-store').redirect('/');
     })
   })
   module.exports = dashRouter;
