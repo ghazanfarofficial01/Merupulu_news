e97a0e9caaa68eefe9ea4d24f66a47380ea73ee9
ghazanfarofficial01 <ghazanfar.official@outlook.com>
2023-09-17 23:06:26 +0530

    commit from local

--- routes/news.js
+++ routes/news.js
@@ -165,8 +165,7 @@ newsRouter.get('/api/news/district', async (req, res) => {
 });
 
 
-//pushing this code
-=======
+//api to fetch news by id
 newsRouter.get('/api/news/:id',async (req,res) => {
   try{
     console.log("reached /:id")
